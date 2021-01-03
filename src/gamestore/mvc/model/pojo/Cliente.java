59d73ba075eccaad90c2855f0ca62e53e46f5a94
Letícia Fernandes <lemayara16@gmail.com>
2021-01-03 15:24:51 -0300

    criando o controller para ClientesDialog

--- src/gamestore/mvc/model/pojo/Cliente.java
+++ src/gamestore/mvc/model/pojo/Cliente.java
@@ -1,13 +1,13 @@
 package gamestore.mvc.model.pojo;
 
 public class Cliente {
-	
+
 	private int clienteId;
 	private int codigo;
 	private String nome;
 	private String endereco;
 	private String outrasInformacoes;
-	
+
 	public Cliente(int clienteId, int codigo, String nome, String endereco, String outrasInformacoes) {
 		super();
 		this.clienteId = clienteId;
@@ -28,7 +28,7 @@ public class Cliente {
 
 
 	public Cliente() {
-	
+
 	}
 
 	public int getClienteId() {
@@ -42,34 +42,34 @@ public class Cliente {
 	public int getCodigo() {
 		return codigo;
 	}
-	
+
 	public Cliente setCodigo(int codigo) {
 		this.codigo = codigo;
 		return this;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public Cliente setNome(String nome) {
 		this.nome = nome;
 		return this;
 	}
-	
+
 	public String getEndereco() {
 		return endereco;
 	}
-	
+
 	public Cliente setEndereco(String endereco) {
 		this.endereco = endereco;
 		return this;
 	}
-	
+
 	public String getOutrasInformacoes() {
 		return outrasInformacoes;
 	}
-	
+
 	public Cliente setOutrasInformacoes(String outrasInformacoes) {
 		this.outrasInformacoes = outrasInformacoes;
 		return this;
